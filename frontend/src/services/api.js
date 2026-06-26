86d78aad7db33a1d170a5663f24a488511cb52ff
AdiKesava-Reddy10 <vtu25083@veltech.edu.in>
2026-06-26 23:05:56 +0530

    Fixed API base URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 // Set backend baseURL. Standard localhost port 5000 as configured on server
-const API_BASE_URL = import.meta.env.VITE_API_URL;
+const API_BASE_URL =
+  import.meta.env.VITE_API_URL + "/api";
 const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
